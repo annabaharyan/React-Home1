4a83316845eb296039dc5bfcfd1488c7e8a2de22
Anna Baharyan <anna.baharyan91@gmail.com>
2022-08-24 13:09:45 +0400

    button click thith useState

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
 import './App.css';
+import {useState} from 'react'
 
-function add1(){
-
-  return document.querySelector('.num').innerHTML++
-
-}
-function add10(){
-
-    let val=+document.querySelector('.num').innerHTML;
-    val+=10;
-    return document.querySelector('.num').innerHTML=val
-  }
-function sub1(){
-
-    return document.querySelector('.num').innerHTML--
-    }
-function sub10(){
-
-      let val=+document.querySelector('.num').innerHTML;
-      val-=10;
-      return document.querySelector('.num').innerHTML=val
-    }
 
 function App() {
-  
+
+let [val, setVal]= useState(0)
+
   return (
-   <div id="container">
-      <div className='num'>0</div>
-      <button className="addOne" onClick={add1}>Add +1</button>
-      <button className="subOne" onClick={sub1}>Substract -1</button>
-      <button  className="addTen"onClick={add10}>Add +10</button>
-      <button  className="subTen"onClick={sub10}>Sub -10</button>
-   </div>
-  );
+    <div id="container">
+          <h1>{val}</h1>
+          <button onClick={()=>setVal(num=>num+1)}>Add+1</button>
+          <button onClick={()=>setVal(num=>num-1)}>Sub-1</button>
+          <button onClick={()=>setVal(num=>num+10)}>Add+10</button>
+          <button onClick={()=>setVal(num=>num-10)}>Sub-10</button>
+    </div>
+  )
+
+
+
+
 }
 
 export default App;
-
